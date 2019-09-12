85dabd05ed4b5fe3e0d658384d497ede00e92f78
ericlima <eric.george.lima@gmail.com>
2019-09-12 16:09:15 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Chart} from 'primereact/chart';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
@@ -13,11 +14,26 @@ class App extends Component {
 
   render() {
     
-    
+    const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: '#42A5F5',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: '#9CCC65',
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]    
+        };
 
     return (
       <div>
-        <Hello/>
+        <a href="#">link</a>   
+        <Chart data={data}/>
       </div>
     );
   }
